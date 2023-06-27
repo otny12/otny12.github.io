36ac16e5b07b118f33a4ec56099da558f90e1a30
otny12 <55300810+otny12@users.noreply.github.com>
2023-06-27 16:51:39 -0500

    Home page(unbalanced)

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const slideContainer = document.getElementById("slide-container");
 const slide = document.querySelector(".slide");
 const leftButton = document.getElementById("slide-arrow-left");
 const rightButton = document.getElementById("slide-arrow-right");
+const projectsButton = document.getElementById("projects-button");
 
 rightButton.addEventListener("click", () => {
   const slideWidth = slide.clientWidth;
@@ -11,4 +12,9 @@ rightButton.addEventListener("click", () => {
 leftButton.addEventListener("click", () => {
   const slideWidth = slide.clientWidth;
   slideContainer.scrollLeft -= slideWidth;
+});
+
+projectsButton.addEventListener("click", () => {
+  const slideWidth = slide.clientWidth;
+  slideContainer.scrollLeft += slideWidth;
 });
